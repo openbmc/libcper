cf17955cbe023598f74219200c054881f2062564
Karthik Rajagopalan <krajagopalan@nvidia.com>
2024-09-06 14:36:34 -0700

    Use extern C guards in headers
    This fixes C++ compiler error over variable-length array members
    ```
    In file included from ../tests/../sections/cper-section.h:7,
                     from ../tests/ir-tests.cpp:15:
    ../tests/../sections/../edk/Cper.h:1405:34: error: ISO C++ forbids flexible array member ‘Register’ [-Werror=pedantic]
     1405 |         EFI_NVIDIA_REGISTER_DATA Register[];
          |                                  ^~~~~~~~
    ```
    
    Change-Id: I835dd05166732ca213c72eae2904815a8769599b
    Signed-off-by: Karthik Rajagopalan <krajagopalan@nvidia.com>

--- edk/Cper.h
+++ edk/Cper.h
@@ -9,10 +9,15 @@
   GUIDs defined in UEFI 2.7 Specification.
 
 **/
-#include "BaseTypes.h"
 
 #ifndef __CPER_GUID_H__
 #define __CPER_GUID_H__
+
+#include "BaseTypes.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 #pragma pack(push, 1)
 
@@ -1407,5 +1412,9 @@ typedef struct {
 
 extern EFI_GUID gEfiNvidiaErrorSectionGuid;
 #pragma pack(pop)
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
