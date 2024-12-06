bba0d1990954b0e4be3d4d26a6a1fae9b9319131
Aushim Nagarkatti <anagarkatti@nvidia.com>
2024-12-05 18:17:27 -0800

    Include hex decode for human readable fields
    Hexadecimal decode for some fields like deviceAddress
    make sense to be represented in hex over decimal to make
    scripting and human-usability easier.
    
    Tested on gb200
    
    Change-Id: I7d0d100162bc681c3c6885ca01ed23020c3b5063
    Signed-off-by: Aushim Nagarkatti <anagarkatti@nvidia.com>

--- sections/cper-section-pci-bus.c
+++ sections/cper-section-pci-bus.c
@@ -52,10 +52,23 @@ json_object *cper_section_pci_bus_to_ir(void *section)
 	json_object_object_add(
 		section_ir, "busCommandType",
 		json_object_new_string(command_type == 0 ? "PCI" : "PCI-X"));
+
 	json_object_object_add(section_ir, "busRequestorID",
 			       json_object_new_uint64(bus_error->RequestorId));
+
+	char hexstring_buf[EFI_UINT64_HEX_STRING_LEN];
+	snprintf(hexstring_buf, EFI_UINT64_HEX_STRING_LEN, "0x%016llX",
+		 bus_error->RequestorId);
+	json_object_object_add(section_ir, "busRequestorIDHex",
+			       json_object_new_string(hexstring_buf));
+
 	json_object_object_add(section_ir, "busCompleterID",
 			       json_object_new_uint64(bus_error->ResponderId));
+	snprintf(hexstring_buf, EFI_UINT64_HEX_STRING_LEN, "0x%016llX",
+		 bus_error->ResponderId);
+	json_object_object_add(section_ir, "busCompleterIDHex",
+			       json_object_new_string(hexstring_buf));
+
 	json_object_object_add(section_ir, "targetID",
 			       json_object_new_uint64(bus_error->TargetId));
 
