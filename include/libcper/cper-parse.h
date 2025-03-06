f4b2cc85f10a0d7d0af171947b0be39e69c9046c
Ed Tanous <etanous@nvidia.com>
2025-03-05 18:03:36 -0800

    WIP: Invert buffer modes
    Currently the libcper core operates on FILE objects, with a number of
    seeks.  Change this such that the internals rely on buffers and lengths
    instead.  This ensures that we are checking records lengths in a given
    CPER header against ranged limits.
    
    Change-Id: Id16902afbf53f091058c3206a473ddd725ec4bf1
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- include/libcper/cper-parse.h
+++ include/libcper/cper-parse.h
@@ -29,7 +29,7 @@ extern "C" {
 			  "HW_ERROR_FLAGS_SIMULATED" }
 
 json_object *cper_to_ir(FILE *cper_file);
-json_object *cper_buf_to_ir(const void *cper_buf, size_t size);
+json_object *cper_buf_to_ir(const unsigned char *cper_buf, size_t size);
 json_object *cper_single_section_to_ir(FILE *cper_section_file);
 json_object *cper_buf_single_section_to_ir(void *cper_buf, size_t size);
 
