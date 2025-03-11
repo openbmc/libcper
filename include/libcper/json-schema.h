9eccd797315e3758d7b9e41af90cac7d522e7960
Ed Tanous <etanous@nvidia.com>
2025-03-10 21:13:46 -0700

    Remove json schema validation
    libcper is a cper parsing library.  It is not a json schema validation
    library.  If we want to add support for this in the CLI, we should use a
    relevant library.  The json schema validation that's here isn't correct
    to the json spec, to the point where it needed to be replaced in unit
    tests.  Adding internal json schema validation doesn't add value here.
    It is trivial to run jsonschema as a cli app, there's not a lot of
    reason to keep it here and take up the binary space.
    
    Change-Id: I076c8eb9f81c34cdfc40525fe940e8fba0f945b3
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- include/libcper/json-schema.h
+++ include/libcper/json-schema.h
@@ -14,8 +14,6 @@ int validate_schema(json_object *schema, char *schema_directory,
 		    json_object *object, char *error_message);
 int validate_schema_from_file(const char *schema_file, json_object *object,
 			      char *error_message);
-void validate_schema_debug_enable();
-void validate_schema_debug_disable();
 
 #ifdef __cplusplus
 }
