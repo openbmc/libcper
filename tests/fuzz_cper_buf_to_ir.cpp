9ad355bb4b86d3fd1260ff40811a9de9c330f4c6
Ed Tanous <ed@tanous.net>
2025-03-13 21:50:18 -0700

    WIP: Try to fix round trip
    Change-Id: I28346f33c4ca7266983071ddcdbc3639eac4d721
    Signed-off-by: Ed Tanous <ed@tanous.net>

--- tests/fuzz_cper_buf_to_ir.cpp
+++ tests/fuzz_cper_buf_to_ir.cpp
@@ -1,28 +1,83 @@
+#include <cassert>
+#include <cstring>
+#include <cstdint>
+#include <cstdlib>
 #include "libcper/cper-parse.h"
-#include "test-utils.hpp"
+#include <string>
+#include <iostream>
+#include <span>
+#include <libcper/Cper.h>
+
+std::string to_hex(char *input, size_t size)
+{
+	std::string out;
+	for (size_t i = 0; i < size; i++) {
+		out += std::format("{:02x}",
+				   static_cast<unsigned char>(input[i]));
+		if (i % 8 == 7) {
+			out += "\n";
+		}
+	}
+	return out;
+}
+
+uint8_t *zero_out_reserved_bits(const uint8_t *input, size_t size)
+{
+	uint8_t *input_copy = (uint8_t *)malloc(size);
+	memcpy(input_copy, input, size);
+	size_t remaining = size;
+	if (remaining < sizeof(EFI_COMMON_ERROR_RECORD_HEADER)) {
+			return NULL;
+	}
+
+	EFI_COMMON_ERROR_RECORD_HEADER *header = NULL;
+	header = (EFI_COMMON_ERROR_RECORD_HEADER *)input;
+	remaining -= sizeof(EFI_COMMON_ERROR_RECORD_HEADER);
+	input += sizeof(EFI_COMMON_ERROR_RECORD_HEADER);
+
+	// Reserved bits beyond the third bit are reserved.
+	header->ValidationBits |= 0X7;
+	return input_copy;
+}
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size)
 {
+	printf("start\n");
 	json_object *ir = cper_buf_to_ir(data, size);
-	if (ir == NULL) {
-		return 0;
-	}
-	char *str = strdup(json_object_to_json_string(ir));
 
-	nlohmann::json jsonData = nlohmann::json::parse(str, nullptr, false);
-	free(str);
-	assert(jsonData.is_discarded() == false);
-	std::string error_message;
-	static std::unique_ptr<valijson::Schema> schema =
-		load_schema(AddRequiredProps::NO, 0);
-
-	int valid = schema_validate_from_file(*schema, jsonData, error_message);
-	if (!valid) {
-		std::cout << "JSON: " << jsonData.dump(4) << std::endl;
-		std::cout << "Error: " << error_message << std::endl;
+	if (ir != NULL) {
+		json_object_put(ir);
 	}
-	assert(valid);
-	json_object_put(ir);
+	uint8_t *data_no_reserved = zero_out_reserved_bits(data, size);
+
+	ir = cper_buf_to_ir(data_no_reserved, size);
+	bool data_same = false;
+	if (ir != NULL) {
+		void *buf = malloc(size);
+		FILE *out = fmemopen(buf, size, "w");
+		ir_to_cper(ir, out);
+
+		fclose(out);
+
+		//Print the input and output.
+
+		std::cout << "json: " << json_object_to_json_string(ir)
+			  << std::endl;
+		std::cout << "Input: \n"
+			  << to_hex((char *)data, size) << std::endl;
+		std::cout << "Output: \n"
+			  << to_hex((char *)buf, size) << std::endl;
+		json_object_put(ir);
+
+
+		data_same = memcmp(data, data_no_reserved, size) == 0;
+		free(data_no_reserved);
+
+		assert(data_same);
+
+	}
+	free(data_no_reserved);
+
 
 	return 0;
 }
