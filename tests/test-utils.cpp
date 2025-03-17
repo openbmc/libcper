fdfce93d587cd73c236b48e3da5c2abde79b00a6
Ed Tanous <etanous@nvidia.com>
2025-03-16 17:40:04 -0700

    Use less c++
    As a design, it would be better if we didn't need to have a dependency
    on c++ in a c library.  Moving things to C will reduce both
    dependencies and compile times.
    
    Reduced dependencies makes the library itself easier to use in
    more environments.
    
    libmctp Is a library that doesn't take a dependency on gtest for its
    tests
    libpldm Is an example that does take a dependency on gtest.
    
    Change-Id: If1dc638f87b75b28181a0baf67f5a18d389cff81
    Signed-off-by: Ed Tanous <etanous@nvidia.com>

--- tests/test-utils.cpp
+++ tests/test-utils.cpp
@@ -4,13 +4,9 @@
  * Author: Lawrence.Tang@arm.com
  **/
 
-#include <cstdio>
-#include <cstdlib>
-#include <fstream>
-#include <map>
-#include <filesystem>
-#include <vector>
-#include <algorithm>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "test-utils.hpp"
 
 #include <libcper/BaseTypes.h>
@@ -22,38 +18,36 @@ extern "C" {
 #include <libcper/log.h>
 }
 
-namespace fs = std::filesystem;
-
 // Objects that have mutually exclusive fields (and thereforce can't have both
 // required at the same time) can be added to this list.
 // Truly optional properties that shouldn't be added to "required" field for
 // validating the entire schema with validationbits=1
 // In most cases making sure examples set all valid bits is preferable to adding to this list
-const static std::vector<std::string> optional_props = {
-	{ // Some sections don't parse header correctly?
-	  "header",
+static const char *optional_props[] = {
+	// Some sections don't parse header correctly?
+	"header",
 
-	  // Each section is optional
-	  "GenericProcessor", "Ia32x64Processor", "ArmProcessor", "Memory",
-	  "Memory2", "Pcie", "PciBus", "PciComponent", "Firmware",
-	  "GenericDmar", "VtdDmar", "IommuDmar", "CcixPer", "CxlProtocol",
-	  "CxlComponent", "Nvidia", "Ampere", "Unknown",
+	// Each section is optional
+	"GenericProcessor", "Ia32x64Processor", "ArmProcessor", "Memory",
+	"Memory2", "Pcie", "PciBus", "PciComponent", "Firmware", "GenericDmar",
+	"VtdDmar", "IommuDmar", "CcixPer", "CxlProtocol", "CxlComponent",
+	"Nvidia", "Ampere", "Unknown",
 
-	  // CXL?  might have a bug?
-	  "partitionID",
+	// CXL?  might have a bug?
+	"partitionID",
 
-	  // CXL protocol
-	  "capabilityStructure", "deviceSerial",
+	// CXL protocol
+	"capabilityStructure", "deviceSerial",
 
-	  // CXL component
-	  "cxlComponentEventLog", "addressSpace", "errorType",
-	  "participationType", "timedOut", "level", "operation", "preciseIP",
-	  "restartableIP", "overflow", "uncorrected", "transactionType",
+	// CXL component
+	"cxlComponentEventLog", "addressSpace", "errorType",
+	"participationType", "timedOut", "level", "operation", "preciseIP",
+	"restartableIP", "overflow", "uncorrected", "transactionType",
 
-	  // PCIe AER
-	  "addressSpace", "errorType", "participationType", "timedOut", "level",
-	  "operation", "preciseIP", "restartableIP", "overflow", "uncorrected",
-	  "transactionType" }
+	// PCIe AER
+	"addressSpace", "errorType", "participationType", "timedOut", "level",
+	"operation", "preciseIP", "restartableIP", "overflow", "uncorrected",
+	"transactionType"
 };
 
 //Returns a ready-for-use memory stream containing a CPER record with the given sections inside.
@@ -67,11 +61,11 @@ FILE *generate_record_memstream(const char **types, UINT16 num_types,
 
 	//Generate a section to the stream, close & return.
 	if (!single_section) {
-		generate_cper_record(const_cast<char **>(types), num_types,
-				     stream, validBitsType);
+		generate_cper_record((char **)(types), num_types, stream,
+				     validBitsType);
 	} else {
-		generate_single_section_record(const_cast<char *>(types[0]),
-					       stream, validBitsType);
+		generate_single_section_record((char *)(types[0]), stream,
+					       validBitsType);
 	}
 	fclose(stream);
 
@@ -79,23 +73,28 @@ FILE *generate_record_memstream(const char **types, UINT16 num_types,
 	return fmemopen(*buf, *buf_size, "r");
 }
 
-int iterate_make_required_props(json_object *jsonSchema, bool all_valid_bits)
+int iterate_make_required_props(json_object *jsonSchema, int all_valid_bits)
 {
 	//properties
 	json_object *properties =
 		json_object_object_get(jsonSchema, "properties");
 
-	if (properties != nullptr) {
+	if (properties != NULL) {
 		json_object *requrired_arr = json_object_new_array();
 
 		json_object_object_foreach(properties, property_name,
 					   property_value)
 		{
-			bool add_to_required = true;
-			const auto it_find_opt_prop = std::ranges::find(
-				optional_props, property_name);
-			if (it_find_opt_prop != optional_props.end()) {
-				add_to_required = false;
+			(void)property_value;
+			int add_to_required = 1;
+			size_t num = sizeof(optional_props) /
+				     sizeof(optional_props[0]);
+			for (size_t i = 0; i < num; i++) {
+				if (strcmp(optional_props[i], property_name) ==
+				    0) {
+					add_to_required = 0;
+					break;
+				}
 			}
 
 			if (add_to_required) {
@@ -112,6 +111,7 @@ int iterate_make_required_props(json_object *jsonSchema, bool all_valid_bits)
 			(void)property_name2;
 			if (iterate_make_required_props(property_value2,
 							all_valid_bits) < 0) {
+				json_object_put(requrired_arr);
 				return -1;
 			}
 		}
@@ -119,23 +119,37 @@ int iterate_make_required_props(json_object *jsonSchema, bool all_valid_bits)
 		if (all_valid_bits) {
 			json_object_object_add(jsonSchema, "required",
 					       requrired_arr);
+		} else {
+			json_object_put(requrired_arr);
 		}
-		//json_object_put(requrired_arr);
 	}
 
 	// ref
 	json_object *ref = json_object_object_get(jsonSchema, "$ref");
-	if (ref != nullptr) {
+	if (ref != NULL) {
 		const char *ref_str = json_object_get_string(ref);
-		if (ref_str != nullptr) {
-			std::string ref_path = LIBCPER_JSON_SPEC;
-			// remove the leading .
-			ref_path += std::string(ref_str).substr(1);
-			json_object *ref_obj =
-				json_object_from_file(ref_path.c_str());
-			if (ref_obj == nullptr) {
-				printf("Failed to parse file: %s\n",
-				       ref_path.c_str());
+		if (ref_str != NULL) {
+			if (strlen(ref_str) < 1) {
+				cper_print_log("Failed seek filepath: %s\n",
+					       ref_str);
+				return -1;
+			}
+			size_t size =
+				strlen(LIBCPER_JSON_SPEC) + strlen(ref_str);
+			char *path = (char *)malloc(size);
+			int n = snprintf(path, size, "%s%s", LIBCPER_JSON_SPEC,
+					 ref_str + 1);
+			if (n != (int)size - 1) {
+				cper_print_log("Failed concat filepath: %s\n",
+					       ref_str);
+				free(path);
+				return -1;
+			}
+			json_object *ref_obj = json_object_from_file(path);
+			free(path);
+			if (ref_obj == NULL) {
+				cper_print_log("Failed to parse file: %s\n",
+					       ref_str);
 				return -1;
 			}
 
@@ -158,7 +172,7 @@ int iterate_make_required_props(json_object *jsonSchema, bool all_valid_bits)
 
 	//oneOf
 	const json_object *oneOf = json_object_object_get(jsonSchema, "oneOf");
-	if (oneOf != nullptr) {
+	if (oneOf != NULL) {
 		size_t num_elements = json_object_array_length(oneOf);
 
 		for (size_t i = 0; i < num_elements; i++) {
@@ -172,7 +186,7 @@ int iterate_make_required_props(json_object *jsonSchema, bool all_valid_bits)
 
 	//items
 	const json_object *items = json_object_object_get(jsonSchema, "items");
-	if (items != nullptr) {
+	if (items != NULL) {
 		json_object_object_foreach(items, key, val)
 		{
 			(void)key;
@@ -195,35 +209,54 @@ int schema_validate_from_file(json_object *to_test, int single_section,
 	} else {
 		schema_file = "cper-json-full-log.json";
 	}
+	size_t size = strlen(LIBCPER_JSON_SPEC) + 1 + strlen(schema_file) + 1;
+	char *schema_path = (char *)malloc(size);
+	if (schema_path == NULL) {
+		cper_print_log("Failed to allocate memory for schema path\n");
+		return -1;
+	}
+	int n = snprintf(schema_path, size, "%s/%s", LIBCPER_JSON_SPEC,
+			 schema_file);
+	if (n != (int)size - 1) {
+		cper_print_log("Failed to format schema path n=%d size=%zu\n",
+			       n, size);
+		cper_print_log("str=%s\n", schema_path);
+		free(schema_path);
+		return -1;
+	}
 
-	std::string schema_path = LIBCPER_JSON_SPEC;
-	schema_path += "/";
-	schema_path += schema_file;
+	json_object *schema = json_object_from_file(schema_path);
 
-	json_object *schema = json_object_from_file(schema_path.c_str());
-	if (schema == nullptr) {
-		cper_print_log("Could not parse schema file: %s", schema_file);
+	if (schema == NULL) {
+		cper_print_log("Could not parse schema file: %s", schema_path);
+		free(schema_path);
 		return 0;
 	}
 
 	if (iterate_make_required_props(schema, all_valid_bits) < 0) {
-		printf("Failed to make required props\n");
+		cper_print_log("Failed to make required props\n");
 		json_object_put(schema);
+		free(schema_path);
 		return -1;
 	}
 
 	int err = jdac_validate(to_test, schema);
 	if (err == JDAC_ERR_VALID) {
-		printf("validation ok\n");
+		cper_print_log("validation ok\n");
 		json_object_put(schema);
+		free(schema_path);
 		return 1;
 	}
-	printf("validate failed %d: %s\n", err, jdac_errorstr(err));
 
-	printf("schema: \n%s\n",
-	       json_object_to_json_string_ext(schema, JSON_C_TO_STRING_PRETTY));
-	printf("to_test: \n%s\n", json_object_to_json_string_ext(
-					  to_test, JSON_C_TO_STRING_PRETTY));
+	cper_print_log("validate failed %d: %s\n", err, jdac_errorstr(err));
+
+	cper_print_log("schema: \n%s\n",
+		       json_object_to_json_string_ext(schema,
+						      JSON_C_TO_STRING_PRETTY));
+	cper_print_log("to_test: \n%s\n",
+		       json_object_to_json_string_ext(to_test,
+						      JSON_C_TO_STRING_PRETTY));
 	json_object_put(schema);
+	free(schema_path);
 	return 0;
 }
