0ccd655e5f58cb709805214dc0e2250a1baa7a24
Aushim Nagarkatti <anagarkatti@nvidia.com>
2026-01-13 16:12:04 -0800

    CMET size check off by 1
    The size calculation was off by 1 in the old code. We need to account
    for EFI_NVIDIA_REGISTER_DATA + EFI_NVIDIA_ERROR_DATA when i==0.
    
    Change-Id: Ib5d29f90cd52a199dd2fa391bac5d4bb634083a6
    Signed-off-by: Aushim Nagarkatti <anagarkatti@nvidia.com>

--- sections/cper-section-nvidia.c
+++ sections/cper-section-nvidia.c
@@ -20,7 +20,7 @@ void parse_cmet_info(EFI_NVIDIA_REGISTER_DATA *regPtr, UINT8 NumberRegs,
 	for (int i = 0; i < NumberRegs; i++, regPtr++) {
 		json_object *reg = NULL;
 		if (sizeof(EFI_NVIDIA_ERROR_DATA) +
-			    i * sizeof(EFI_NVIDIA_REGISTER_DATA) <
+			    (i + 1) * sizeof(EFI_NVIDIA_REGISTER_DATA) <=
 		    size) {
 			reg = json_object_new_object();
 			add_int_hex_64(reg, "ChannelAddress", regPtr->Address);
@@ -80,7 +80,7 @@ void parse_registers(EFI_NVIDIA_REGISTER_DATA *regPtr, UINT8 NumberRegs,
 	for (int i = 0; i < NumberRegs; i++, regPtr++) {
 		json_object *reg = NULL;
 		if (sizeof(EFI_NVIDIA_ERROR_DATA) +
-			    i * sizeof(EFI_NVIDIA_REGISTER_DATA) <
+			    (i + 1) * sizeof(EFI_NVIDIA_REGISTER_DATA) <=
 		    size) {
 			reg = json_object_new_object();
 			json_object_object_add(
